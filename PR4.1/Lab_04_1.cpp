65b27c18892518219b095f836c8ebef19cbbe3e7
unknown <deadmarvald@gmail.com>
2021-10-05 22:04:32 +0300

    Лабораторна робота №4.1

---
+++ PR4.1/Lab_04_1.cpp
@@ -0,0 +1,63 @@
+// Lab_04_1.cpp
+// Вольвенко Іван 
+// Лабораторна робота № 4.1
+// Цикли
+// Варіант 13
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int k, N;
+	double P;
+
+	cout << "N = "; cin >> N;
+
+	P = 1;
+	k = N;
+
+	while (k <= 25)
+	{
+		P *= sqrt(1 + N / k);
+		k++;
+	}
+	cout << P << endl;
+
+	P = 1;
+	k = N;
+
+	do {
+		P *= sqrt(1 + N / k);
+		k++;
+	} while (k <= 25);
+	cout << P << endl;
+
+	P = 1;
+	
+	for (k = N; k <= 25; k++)
+	{
+		P *= sqrt(1 + N / k);
+	}
+	cout << P << endl;
+	
+	P = 1;
+
+	for (k = 25; k >= N; k--)
+	{
+		P *= sqrt(1 + N / k);
+	}
+	cout << P << endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
